28606ef1423f0464cbea03ba1ab7e2a1a7044ffd
Yuri <140124162+Yuzodin@users.noreply.github.com>
2023-11-18 09:15:13 -0300

    Add files via upload
    Um trabalho feito em python que mostra meus treinos para cada dia da semana sendo domingo dia de descanço.

---
+++ Projeto pessoal dia de academia.py
@@ -0,0 +1,96 @@
+print('-='*31)
+print("FICHA de treino semanal pessoal de treino A,B E C (Seg - Sab)")
+print('-='*31)
+r = 0
+dia = str(input('Qual dia da semana é hoje?:')).upper().strip()
+if dia == 'SEGUNDA':
+    print('Hoje é dia de treino de Peito e Triceps na academia')
+    res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+    if res == 'S':
+        print()
+        print('''LISTA DE EXERCISIOS SEGUNDA
+    1 - Supino 30 graus
+    2 - Crucifixo
+    3 - Desevolvimento Maquina
+    4 - Crossover
+    5- Triceps Pulley
+    6- Elevação Lateral.''')
+        print('='*30)
+
+elif dia == 'TERÇA':
+        print('Hoje é dia de treino de Costas e Biceps na academia')
+        res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+        if res == 'S':
+            print()
+            print('''LISTA DE EXERCISIOS TERÇA
+    1 - Pulley Frente
+    2 - Remada Baixa
+    3 - Cruxifixo Invertido
+    4 - Rosca Direta
+    5- Rosca Martelo
+    6- Encolhimento Trapezio.''')
+            print('='*30)
+
+elif dia == 'QUARTA':
+    print('Hoje é dia de treino de Perna na academia')
+    res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+    if res == 'S':
+        print()
+        print('''LISTA DE EXERCISIOS QUARTA
+    1 - Agachamento
+    2 - Cadeira Extensora
+    3 - Mesa
+    4 - Panturrilha
+    5- Cadeira Abdutora e Adutora
+    6- Abdominal Maquina.''')
+        print('='*30)
+
+    elif dia == 'QUINTA':
+        print('Hoje é dia de treino de Peito e Triceps na academia')
+        res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+        if res == 'S':
+            print()
+            print('''LISTA DE EXERCISIOS QUINTA
+    1 - Supino 30 graus
+    2 - Crucifixo
+    3 - Desevolvimento Maquina
+    4 - Crossover
+    5- Triceps Pulley
+    6- Elevação Lateral.''')
+            print('='*30)
+
+    elif dia == 'SEXTA':
+        print('Hoje é dia de treino de Costas e Biceps na academia')
+        res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+        if res == 'S':
+            print()
+            print('''LISTA DE EXERCISIOS SEXTA
+    1 - Pulley Frente
+    2 - Remada Baixa
+    3 - Cruxifixo Invertido
+    4 - Rosca Direta
+    5- Rosca Martelo
+    6- Encolhimento Trapezio.''')
+            print('='*30)
+
+    elif dia == 'SABADO':
+        print('Hoje é dia de treino de Perna na academia')
+        res = str(input('Deseja ver a lista de exercios desse dia?:[S/N]: ')).strip().upper()
+        if res == 'S':
+            print()
+            print('''LISTA DE EXERCISIOS QUARTA
+    1 - Agachamento
+    2 - Cadeira Extensora
+    3 - Mesa
+    4 - Panturrilha
+    5- Cadeira Abdutora e Adutora
+    6- Abdominal Maquina.''')
+            print('='*30)
+
+    elif dia == 'DOMINGO':
+        print('Descanse você merece Campeão. Segunda voltamos a todo vapor!')
+
+    else:
+        print('Não reconheci qual dia solicitado tente novamente.')       
+
+print('Obrigando por usar nosso progama de treino volte sempre é bom treino!')
