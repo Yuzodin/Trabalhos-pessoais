dac83cdc52c4fc0ab4f7742bd689c04eba6b2c5e
Yuri <140124162+Yuzodin@users.noreply.github.com>
2024-02-28 08:34:02 -0300

    Add files via upload

---
+++ El projeto/src/App.js/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        "anime": "bleach",
+        "name": ['Kurosaki Ichigo', 'Zaraki Kenpachi', 'Urahara Kisuke', 'Aizen Sousuke', 'Hitsugaya Toushirou', 'Kuchiki Rukia', 'Kuchiki Byakuya', 'Jaegerjaquez Grimmjow', 'Shihouin Yoruichi', 'Ichimaru Gin'],
+        "desc": [
+            'Na maior parte do tempo, Ichigo parece um adolescente normal, a única exceção é seu cabelo espetado e laranja, uma característica que ele tem sido ridicularizado por anos. Ele é uma pessoa bastante alta e magra, com pele de pêssego e olhos castanhos. Quando em sua forma espiritual, Ichigo usa traje Shinigami padrão com a adição de uma alça em seu peito.',
+            'O sobrenome de Zaraki vem do distrito em que ele residia, Zaraki, que é o distrito #80 do Beco Norte de Rukongai. Enquanto residia no Distrito #80, Kenpachi não tinha nome. Durante esse tempo, ele se tornou proficiente em esgrima e matou inúmeros inimigos.',
+            'Kisuke Urahara sempre usa sandálias de madeira japonesas tradicionais e um bucket hat, o que lhe rendeu o apelido de "sandal-hat". Ele geralmente é um sujeito descontraído e jovial, embora possa estar morto quando a situação o justificar.',
+            'Aizen é um intelectual educado e de fala mansa, muitas vezes se dirigindo a seus subordinados por seus nomes próprios. Ele raramente mostra qualquer sinal de alarme ou angústia e muitas vezes tem situações inteiras planejadas com bastante antecedência.',
+            'Hitsugaya vem de Junrinan, Distrito 1 de Rukongai. Lá, ele morava com uma idosa, Momo Hinamori, e possivelmente outras pessoas. Pouco mais se sabe sobre seu passado, mas sabe-se que ele se tornou um Shinigami logo após Hinamori.',
+            'Rukia e sua irmã mais velha, Hisana, foram reunidas no 78º Distrito de Rukongai depois que morreram no mundo humano. Hisana abandonou Rukia quando ela ainda era um bebê, já que ela não podia continuar a sustentar tanto a si mesma quanto a Rukia.',
+            'Byakuya Kuchiki é o capitão da 6ª Divisão no Gotei 13. Seu tenente é Renji Abarai. Ele é o 28º chefe do nobre clã Kuchiki, uma das quatro famílias nobres da Soul Society. Como tal, Byakuya age de forma aristocrática: está sempre quieto e apático em relação às outras pessoas. ',
+            'Grimmjow tem cabelos azuis claros, e seus olhos têm linhas azuis/verdes abaixo deles que são semelhantes às marcas vermelhas nos olhos de Ikkaku Madarame. O traje de Grimmjow consiste em um hakama branco e uma faixa preta, sua jaqueta branca é esfarrapada e consiste em uma gola virada para cima, com forro interno preto, as mangas são enroladas e Grimmjow a usa aberta; peito revelado.',
+            'Yoruichi tende a evitar batalhas, preferindo ajudar depois que uma luta termina, mas ela intervirá se achar que seu lado não pode vencer. Ela foi responsável por treinar Chad e Orihime antes de sua entrada na Soul Society.',
+            'Como Gin não demonstra nenhuma emoção ao falar combinado com seu uso de sarcasmo, ele é desconfiado por muitos, mais notavelmente Toshiro Hitsugaya e Rukia Kuchiki. Rukia comentou uma vez sobre como ela se sentia como cobras estavam enrolando em seu pescoço sempre que Gin falava.'
+        ],
+        "tokens": ['1t.png', '2t.png', '3t.png', '4t.png', '5t.png', '6t.png', '7t.png', '8t.png', '9t.png', '10t.png'],
+        "wpp": ['1w.png', '2w.png', '3w.jpg', '4w.jpg', '5w.jpg', '6w.png', '7w.jpg', '8w.png', '9w.png', '10w.jpg'],
+        "video": "1w.mp4"
+    },
+    {
+        "anime": "jujutsukaisen",
+        "name": ['Satoru Gojo', 'Yuji Itadori', 'Megumi Fushiguro', 'Nobara Kugisake', 'Ryomen Sukuna', 'Kento Nanami', 'Maki Zenin', 'Toge Inumaki', 'Yuta Okkutsu', 'Aoi Todo'],
+        "desc": [
+            'Satoru Gojo é um dos principais protagonistas da série Jujutsu Kaisen. Ele é um feiticeiro de jujutsu de grau especial e amplamente reconhecido como o mais forte do mundo.',
+            'Yuji Itadori é o principal protagonista da série Jujutsu Kaisen. Ele é filho de Jin Itadori e Kaori Itadori. Yuji é neto de Wasuke Itadori que vivia uma vida normal até encontrar Megumi e comer um dos dedos de Sukuna',
+            'Megumi Fushiguro é o deuteragonista da série Jujutsu Kaisen. Ele é um feiticeiro de jujutsu de grau 2 e aluno do primeiro ano da Tokyo Jujutsu High ao lado de Yuji Itadori e Nobara Kugisaki. Megumi é meio-irmão de Tsumiki Fushiguro e filho de Toji Fushiguro. , Megumi também é descendente da Família Zenin.',
+            'Nobara Kugisaki é a tritagonista da série Jujutsu Kaisen. Ela é uma estudante do primeiro ano e feiticeira de jujutsu da 3ª série na Tokyo Jujutsu High, estudando com Satoru Gojo ao lado de Yuji e Megumi.',
+            'Ryomen Sukuna, mais conhecido simplesmente como Sukuna, é o feiticeiro de jujutsu mais forte de mil anos atrás. Considerado o indiscutível Rei das Maldições, Sukuna é um dos principais antagonistas da série Jujutsu Kaisen.',
+            'Kento Nanami é um personagem coadjuvante em Jujutsu Kaisen. Ele era um ex-aluno da Tokyo Jujutsu High, onde foi aluno do primeiro ano de Satoru Gojo e Suguru Geto. Nanami inicialmente deixou a Jujutsu High depois de se formar para se tornar um assalariado, mas retornou quatro anos depois para continuar. trabalhando como feiticeiro de jujutsu.',
+            'Maki Zenin é uma personagem coadjuvante importante em Jujutsu Kaisen e uma das principais protagonistas de seu prequel, Jujutsu Kaisen 0: Jujutsu High. Ela está atualmente no segundo ano da Tokyo Jujutsu High. Filha de Ogi Zenin e irmã gêmea mais velha de Mai. Zenin, Maki nasceu na família Zenin.',
+            'Toge Inumaki é um personagem coadjuvante importante em Jujutsu Kaisen e um dos principais protagonistas de seu prequel, Jujutsu Kaisen 0: Jujutsu High. Ele é um descendente do clã Inumaki e herdou a fala amaldiçoada do clã, que desenvolveu sua própria fala única. padrão para evitar amaldiçoar os outros.Toge é atualmente um feiticeiro de jujutsu semi-série 1 e estudante do segundo ano na Tokyo Jujutsu High ao lado de seus colegas de classe Yuta, Maki e Panda.',
+            'Yuta Okkotsu é um importante personagem coadjuvante da série Jujutsu Kaisen e o principal protagonista de sua série prequela, Jujutsu Kaisen 0: Jujutsu High. Ele era inicialmente um humano amaldiçoado de grau especial, assombrado por sua falecida amiga de infância, Rika Orimoto. Satoru Gojo foi o mentor de Yuta. e matriculou-o na Tokyo Jujutsu High',
+            'Aoi Todo é um personagem coadjuvante da série Jujutsu Kaisen. Ele é um estudante do terceiro ano da Kyoto Jujutsu High School e um feiticeiro de jujutsu de grau 1. Ele foi orientado por Yuki Tsukumo em sua juventude e considera Yuji Itadori seu melhor amigo.'
+        ],
+        "tokens": ['1t.png', '2t.png', '3t.png', '4t.png', '5t.png', '6t.png', '7t.png', '8t.png', '9t.png', '10t.png'],
+        "wpp": ['1w.png', '2w.png', '3w.jpg', '4w.jpg', '5w.jpg', '6w.jpg', '7w.png', '8w.jpg', '9w.png', '10w.jpg'],
+        "video": '1w.mp4'
+    },
+    {
+        "anime": "onepiece",
+        "name": ['Monkey D. Luffy', 'Roronoa Zoro', 'Nami', 'Usopp', 'Vinsmoke Sanji', 'Tony Tony Chopper', 'Nico Robin', 'Franky', 'Brook', 'Jinbe'],
+        "desc": [
+            'Monkey D. Luffy, também conhecido como \“Luffy do Chapéu de Palha\” e comumente como \“Chapéu de Palha\”, é o fundador e capitão dos cada vez mais infames e poderosos Piratas do Chapéu de Palha, bem como o mais poderoso de seus melhores lutadores. Ele deseja encontrar o tesouro lendário deixado para trás pelo falecido Gol D. Roger e assim se tornar o Rei dos Piratas.',
+            'Roronoa Zoro, também conhecido como \"Caçador de Piratas\" Zoro, é o combatente dos Piratas do Chapéu de Palha e um ex-caçador de recompensas. Ele foi o primeiro membro a juntar-se à tripulação.',
+            'A \"Gata Ladra\" Nami é a navegadora dos Piratas do Chapéu de Palha. Ela foi o terceiro membro da tripulação e a segunda a entrar, fazendo-o durante o Arco Orange Town. Ela é a irmã adotiva de Nojiko, desde que as duas ficaram órfãs e foram levadas por Bell-mère.',
+            '\"God\" Usopp é o Atirador dos Piratas do Chapéu de Palha. Ele é o quarto membro da tripulação e o terceiro a entrar, fazendo isso no final do Arco Vila Syrup. Embora ele tenha deixado a tripulação durante o Arco Water 7, ele se juntou novamente ao final do Arco Pós-Enies Lobby.',
+            '\"Perna Preta\" Sanji, nascido como Vinsmoke Sanji, e também é o cozinheiro dos Piratas do Chapéu de Palha, assim como um antigo chefe de cozinha do Baratie. Ele também é o terceiro filho e a quarta criança da Família Vinsmoke, tornando-o um príncipe do Reino Germa, até que ele oficialmente os renunciou duas vezes.',
+            'Tony Tony Chopper é uma rena. Ele é o médico da tripulação dos Piratas do Chapéu de Palha. Chopper é uma rena que, após comer a Hito Hito no Mi, adquiriu a habilidade de se transformar e raciocinar como os humanos. Ele é o sexto membro e um dos poucos membros da tripulação que nasceu na Grand Line, numa ilha chamada Drum',
+            'Nico Robin, também conhecida como \"Criança Demônio\" e \"Luz da Revolução\", é a arqueóloga dos Piratas do Chapéu de Palha. Ela é a única sobrevivente da ilha destruída de Ohara, localizada no West Blue. Como resultado, ela é atualmente a única pessoa no mundo com a capacidade de ler e decifrar Poneglyphs, uma habilidade que é considerada proibida e que ameaça o Governo Mundial.',
+            'Franky é o carpinteiro dos Piratas do Chapéu de Palha. Ele é um cyborg de 36 anos de Water 7 e foi introduzido na história como o líder da Família Franky, um grupo de desmantelamento de navios. Ele foi originalmente chamado de \"Cutty Flam\" até trocar seu nome original por seu apelido a pedidos de Iceburg para esconder sua identidade.',
+            'Brook é o Músico dos Piratas do Chapéu de Palha. Ele é um esqueleto que os Chapéus de Palha encontraram a bordo de um navio fantasma depois de entrarem no Triângulo Florian. Ele é um usuário de Akuma no Mi que comeu a Yomi Yomi no Mi, o que o torna um pseudo-imortal por causa de seus poderes.',
+            '\"Cavaleiro do Mar\" Jinbe é o timoneiro dos Piratas do Chapéu de Palha. Ele é o décimo membro da tripulação e o nono a se juntar, fazendo isso durante o Arco País de Wano. Jinbe é um homem-peixe sendo um tubarão-baleia e um poderoso mestre do karatê Homem-Peixe. Seu sonho é realizar o desejo moribundo de seu ex-capitão Fisher Tiger de coexistência e igualdade entre humanos e homens-peixe.'
+        ],
+        "tokens": ['1t.png', '2t.png', '3t.png', '4t.png', '5t.png', '6t.png', '7t.png', '8t.png', '9t.png', '10t.png'],
+        "wpp": ['1w.png', '2w.jpg', '3w.jpg', '4w.png', '5w.jpg', '6w.png', '7w.jpg', '8w.png', '9w.png', '10w.jpg'],
+        "video": "1w.mp4"
+    }
+]
