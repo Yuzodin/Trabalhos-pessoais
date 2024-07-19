b3c438c1792dde5e1a575b18419bb500c7d57213
Yuri <140124162+Yuzodin@users.noreply.github.com>
2024-07-19 16:25:18 -0300

    Add files via upload

--- Sakura Shuji e Grill/Sakura Shuji e Grill/assets/js/rolagem.js
+++ Sakura Shuji e Grill/Sakura Shuji e Grill/assets/js/rolagem.js
@@ -1,18 +1,1 @@
-console.log('Unico sempre ativão')
-
-const every = document.querySelectorAll('.hidden')
-
-//aqi se adiciona intersetion observer
-const observer = new IntersectionObserver((vigilante) => {
-    vigilante.forEach ((entrada) => {
-        if(entrada.isIntersecting){
-            entrada.target.classList.add('show')
-        }else{
-            entrada.target.classList.remove('show')
-        }
-        console.log(vigilante)
-    })
-})
-
-//Aqui adiciona o elemento que você quer que ele observe
-every.forEach((elements) => observer.observe(elements))
+console.log("Unico sempre ativ\xE3o");const every=document.querySelectorAll(".hidden"),observer=new IntersectionObserver(a=>{a.forEach(b=>{b.isIntersecting?b.target.classList.add("show"):b.target.classList.remove("show"),console.log(a)})});every.forEach(a=>observer.observe(a));
